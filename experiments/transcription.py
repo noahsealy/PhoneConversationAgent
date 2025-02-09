e2ff40f2b722ece8dbc43c46087e82ddd44a10b7
noahsealy <noah.sealy8@gmail.com>
2025-02-09 17:42:21 -0400

    fixing path

--- experiments/transcription.py
+++ experiments/transcription.py
@@ -9,7 +9,7 @@ print("API Key:", OPENAI_API_KEY)
 
 client = OpenAI()
 
-audio_file= open("/path/to/file/audio.mp3", "rb")
+audio_file= open("audio_input/harvard.wav", "rb")
 transcription = client.audio.transcriptions.create(
     model="whisper-1", 
     file=audio_file
